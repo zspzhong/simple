0b22756bf570eeb6d941a0e3ea7401025d00a6e3
shasharoman <shasharoman@gmail.com>
2015-01-20 23:19:27 +0800

    爬取进度记录

--- src/spider.js
+++ src/spider.js
@@ -11,9 +11,18 @@ function spiderStart() {
 	var pageUrlList = ['https://500px.com/popular'];
 	var imageSrcList = [];
 
-	var writeStart = 0;
-	var oneTimesWriteCount = 20;
-	var urlLimit = 100;
+	var imgWriteStart = 0;
+	var imgOneTimesWriteCount = 20;//每爬取到多少个img记录一次
+
+	var urlWriteStart = 0;
+	var urlOneTimesWriteCount = 50;//每爬取到多少个url记录一次
+	var urlLimit = 100;//url爬取上限
+
+	var currentSpiderIndex = 0;
+	var spiderCounterCircle = 0;//爬取循环计数器
+	var spiderUrlOneTimesCount = 5;//分析多少个url记录一次，用于支持续爬
+
+	var queue = null;
 
 	_initWorkerQueue();
 
@@ -21,26 +30,42 @@ function spiderStart() {
 
 	function _initWorkerQueue() {
 		var concurrency = 10;
-		var queue = async.queue(_spiderOne, concurrency);
+		queue = async.queue(_spiderOne, concurrency);
 
 		queue.push('https://500px.com/popular');
 
-		var start = new Date().getTime();
 		queue.drain = function() {
-			console.log('total url count: ' + pageUrlList.length);
-			console.log('total image count: ' + imageSrcList.length);
-			console.log('total cost time: ' + (new Date().getTime() - start));
+			var imgReadyToWrite = imageSrcList.slice(imgWriteStart);
+			fs.appendFileSync(global.appDir + '/output/image', imgReadyToWrite.join('\n') + '\n');
+
+			var urlReadyToWrite = pageUrlList.slice(urlWriteStart);
+			fs.appendFileSync(global.appDir + '/output/url', urlReadyToWrite.join('\n') + '\n');
+
+			console.log('spider done');
 		};
 	}
 
 	function _spiderOne(url, callback) {
+		console.log('spider ' + url + ' start');
 		browserRequest.request(url, function (err, html) {
+			console.log('spider ' + url + ' end');
+
 			if (err) {
-				console.log(err);
+				console.error(err);
 				callback(err);
 				return;
 			}
 
+			_extractFromHtml(url, html);
+
+			_writeResult2File();
+			_writeUrl2File();
+			_writeProcess2File();
+
+			callback(null);
+		});
+
+		function _extractFromHtml(url, html) {
 			var parseResult = findLinkAndImg(url, html);
 
 			imageSrcList = _.uniq(imageSrcList.concat(parseResult.image));
@@ -54,24 +79,55 @@ function spiderStart() {
 			if (pageUrlList.length <= urlLimit) {
 				queue.push(withoutRepeat);
 			}
-
-			_writeResult2File();
-			callback(null);
-		});
+		}
 
 		function _writeResult2File() {
-			var writeEnd = writeStart + oneTimesWriteCount;
+			var writeEnd = imgWriteStart + imgOneTimesWriteCount;
 			if (imageSrcList.length < writeEnd) {
 				return;
 			}
 
-			var readyToWrite = imageSrcList.slice(writeStart, writeEnd);
+			var imgReadyToWrite = imageSrcList.slice(imgWriteStart, writeEnd);
+			fs.appendFileSync(global.appDir + '/output/image', imgReadyToWrite.join('\n') + '\n');
 
-			fs.appendFileSync(global.appDir + '/output/result', readyToWrite.join('\n') + '\n');
-
-			writeStart = writeEnd;
+			imgWriteStart = writeEnd;
 
 			console.log('write success, image count: ' + writeEnd);
+		}
+
+		function _writeUrl2File() {
+			var writeEnd = urlWriteStart + urlOneTimesWriteCount;
+
+			if (pageUrlList.length < writeEnd) {
+				return;
+			}
+
+			var urlReadyToWrite = pageUrlList.slice(urlWriteStart, writeEnd);
+			fs.appendFileSync(global.appDir + '/output/url', urlReadyToWrite.join('\n') + '\n');
+
+			urlWriteStart = writeEnd;
+
+			console.log('write success, url count: ' + writeEnd);
+		}
+
+		function _writeProcess2File() {
+			currentSpiderIndex++;
+			spiderCounterCircle++;
+
+			if (spiderCounterCircle % spiderUrlOneTimesCount !== 0) {
+				return;
+			}
+
+			var processStr = JSON.stringify({
+				current: currentSpiderIndex,
+				totalImgCount: imageSrcList.length,
+				totalUrlCount: pageUrlList.length
+			});
+
+
+			fs.writeFileSync(global.appDir + '/output/process', processStr);
+
+			console.log('write success, process: ' + processStr);
 		}
 	}
 }
