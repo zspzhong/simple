54108f36d44babc5aabdb0a24bb0d6eb8e5f4f2e
shasharoman <shasharoman@gmail.com>
2015-02-03 23:22:09 +0800

    启动文件配置修改

--- bin/start.sh
+++ bin/start.sh
@@ -1,1 +1,1 @@
-nohup node ../src/master.js -c ../conf/config.json 1>../logs/access.log 2>../logs/error.log &
+nohup node ../lib/master.js -c ../conf/config.json 1>../logs/access.log 2>../logs/error.log &
