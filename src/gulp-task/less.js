5d4999b1be6ff905ba8fed2b5135e842bb21e88b
shasharoman <shasharoman@gmail.com>
2015-11-25 23:01:26 +0800

    css less顺序依赖问题未解决、、、

--- src/gulp-task/less.js
+++ src/gulp-task/less.js
@@ -3,31 +3,26 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var filter = require('gulp-filter');
-var gulpCopy = require('gulp-copy');
 
 var replaceStatic = rename(function (path) {
     path.dirname = path.dirname.replace('/static', '');
     return path;
 });
 
-gulp.task('css-copy', function () {
+gulp.task('less-build', function () {
+    var lessFilter = filter('*.less', {restore: true});
+
     var fileList = [
+        'src/common/**/*.less',
+        'src/**/static/css/*.less',
         'src/**/static/css/*.css',
         'src/common/**/*.css'
     ];
 
     return gulp.src(fileList, {base: 'src'})
-        .pipe(gulpCopy('build/'));
-});
-
-gulp.task('less-build', ['css-copy'], function () {
-    var fileList = [
-        'src/**/static/css/*.less',
-        'src/common/**/*.less'
-    ];
-
-    return gulp.src(fileList, {base: 'src'})
+        .pipe(lessFilter)
         .pipe(less())
+        .pipe(lessFilter.restore)
         .pipe(replaceStatic)
         .pipe(gulp.dest('build/'));
 });
