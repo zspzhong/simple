699943cba79f5762f82d930bdfcb7e9bd74b7e46
shasharoman <shasharoman@gmail.com>
2015-11-25 22:48:32 +0800

    copy css 和parse less 有顺序依赖

--- src/gulp-task/less.js
+++ src/gulp-task/less.js
@@ -3,26 +3,31 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var rename = require('gulp-rename');
 var filter = require('gulp-filter');
+var gulpCopy = require('gulp-copy');
 
 var replaceStatic = rename(function (path) {
     path.dirname = path.dirname.replace('/static', '');
     return path;
 });
 
-gulp.task('less-build', function () {
-    var lessFilter = filter('*.less', {restore: true});
-
+gulp.task('css-copy', function () {
     var fileList = [
-        'src/**/static/css/*.less',
         'src/**/static/css/*.css',
-        'src/common/**/*.less',
         'src/common/**/*.css'
     ];
 
     return gulp.src(fileList, {base: 'src'})
-        .pipe(lessFilter)
+        .pipe(gulpCopy('build/'));
+});
+
+gulp.task('less-build', ['css-copy'], function () {
+    var fileList = [
+        'src/**/static/css/*.less',
+        'src/common/**/*.less'
+    ];
+
+    return gulp.src(fileList, {base: 'src'})
         .pipe(less())
-        .pipe(lessFilter.restore)
         .pipe(replaceStatic)
         .pipe(gulp.dest('build/'));
 });
