874b460ae053d33274db1f801c43112bd97010a8
shasharoman <shasharoman@gmail.com>
2015-11-28 22:35:10 +0800

    git hub web hook

---
+++ src/deploy/service/deploy.js
@@ -0,0 +1,17 @@
+var logger = global.logger;
+var exec = require('child_process').exec;
+
+exports.gitWebHook = gitWebHook;
+
+function gitWebHook(req, res, callback) {
+    exec('git pull origin master', function (err) {
+        if (err) {
+            logger.error(err);
+            callback(err);
+            return;
+        }
+
+        logger.info('git pull 执行成功');
+        callback(null);
+    });
+}
