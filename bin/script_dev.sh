515a7a9167db1ef00016b731e1c4348c85721c82
shasharoman <shasharoman@gmail.com>
2016-01-15 19:10:29 +0800

    bash 说明

--- bin/script_dev.sh
+++ bin/script_dev.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 node /Users/shasharoman/private/simple/lib/runScript.js -c /Users/shasharoman/private/simple/conf/script_config_dev.json -a $1
--- bin/webApp_dev.sh
+++ bin/webApp_dev.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 node --debug=5858 /Users/shasharoman/private/simple/lib/webApp.js -c /Users/shasharoman/private/simple/conf/webApp_config_dev.json
